d1ef6d968fd02f496aad618900c54ffe25bcb7a9
Александр <kobzar.sasha2010@yandex.ru>
2025-02-08 20:25:24 +0500

    Реализован пункт 2.5 полностью

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 class Order(BaseModel):
     number : int
     startDate : datetime.date
+    endDate: Optional[datetime.date] = None
     device :str
     problemType : str
     description : str
@@ -86,6 +87,7 @@ def update_order(dto : Annotated[UpdateOrderDTO, Form()]):
                 message += f"Статус заявки №{o.number} изменен\n"
                 if(o.status == "выполнено"):
                     message += f"Заявка №{o.number} завершена\n"
+                    o.endDate = datetime.datetime.now()
             if dto.description != "":
                 o.description = dto.description
             if dto.master != "":
@@ -93,4 +95,32 @@ def update_order(dto : Annotated[UpdateOrderDTO, Form()]):
             if dto.comment != None and dto.comment != "":
                 o.comments.append(dto.comment)
             return o
-    return "Не найденo"    
+    return "Не найденo" 
+ 
+def complete_count():
+    return len([o for o in repo if o.status == "выполнено"])
+
+def get_problem_type_stat():
+    dict = {}
+    for o in repo:
+        if o.problemType in dict.keys():
+            dict[o.problemType] += 1
+        else:
+            dict[o.problemType] = 1
+    return dict
+
+def get_average_time_to_complete():
+    times = [(
+        datetime.datetime.fromisoformat(o.endDate.isoformat()) -
+        datetime.datetime.fromisoformat(o.startDate.isoformat())).days 
+                 for o in repo 
+                 if o.status == "выполнено"]
+    if complete_count() != 0:
+        return sum(times) / complete_count()
+    return 0
+    
+@app.get("/statistics")
+def get_statistics():
+    return {"complete_count" : complete_count(),
+        "problem_type_stat" : get_problem_type_stat(),
+        "average_time_to_complete" : get_average_time_to_complete()}   
